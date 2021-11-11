804ded99168e031e8f554369c038f14dba4cda70
PabloSilvaAdones <pablo.silva.adones@gmail.com>
2021-11-11 19:54:08 -0300

    login page

--- proyecto_progra/src/app/app-routing.module.ts
+++ proyecto_progra/src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   },{
     path: ':id',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   }
 ];
 
